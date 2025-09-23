ac82085e2dad8b12d08793bb42bf47e3198b0ce5
KirillBaranov <kirillBaranovJob@yandex.ru>
2025-09-23 21:23:34 +0300

    feat[commands]: make registerBuiltinCommands idempotent
    Add module-level guard to prevent duplicate command registration
    when registerBuiltinCommands is called multiple times.

---
+++ packages/commands/src/register.ts
@@ -0,0 +1,17 @@
+import { registry } from "./registry";
+import { hello } from "./commands/hello";
+import { version } from "./commands/version";
+import { diagnose } from "./commands/diagnose";
+import { initProfile } from "./commands/init-profile";
+
+let _registered = false;
+
+export function registerBuiltinCommands() {
+  if (_registered) return;
+  _registered = true;
+  
+  registry.register(hello);
+  registry.register(version);
+  registry.register(diagnose);
+  registry.register(initProfile);
+}
